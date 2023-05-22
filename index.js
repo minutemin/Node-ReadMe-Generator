2db38452e514a105e60215e680f459e9a6f5085a
sarahm <minutemin@gmail.com>
2023-05-21 17:23:32 -0700

    edit typo in readme

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown.js")
+const generateMarkdown = require("./utils/generateMarkdown.js");
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
